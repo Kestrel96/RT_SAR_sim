1303abb1334eede9d11be7439a205057e4de6a52
Kestrel <kuba.poltorak@gmail.com>
2023-09-12 15:19:03 +0200

    updated main

--- main.m
+++ main.m
@@ -34,34 +34,6 @@ azimuth_step=azimuth_distance/azimuth_samples;
 radar=radar_object(B,T,fc,v,PRI,ant_angle,fb_max);
 %%Bd=2*v/radar.lambda*deg2rad(ant_angle);
 
-%% Targets
-
-t1=point_target(10,45);
-t2=point_target(120,35);
-t3=point_target(120,azimuth_distance/2);
-t4=point_target(230,15);
-t5=point_target(120,60);
-t6=point_target(21,58);
-t7=point_target(20,58);
-t8=point_target(250,90);
-t9=point_target(1450,120);
-
-t10=point_target(1450,100);
-t11=point_target(250,150);
-%sample 126
-
-targets=[t1,t2,t3,t4,t5,t6,t7,t8,t9,t10];
-%targets=[t1,t2,t3,t5];
-
-%targets=[t10,t11];
-
-
-
-% Determine antenna length for every target
-for k=1:length(targets)
-    targets(k)=targets(k).get_ant_width(ant_angle);
-end
-
 %% Axes
 faxis=0:fs/samples:fs-1/fs;
 rd_axis=-PRF/2:PRF/azimuth_samples:PRF/2-1/PRF; %Range-Doppler domain axis (azimuth as frequency)
@@ -69,11 +41,68 @@ raxis=faxis*c*T/(2*B);
 azimuth_axis=0:azimuth_step:azimuth_distance-azimuth_step;
 sample_axis=1:1:samples;
 
-%% Sensing
-sensing
+% %% Sensing
+% sensing
+
+%% Real Data
+[raw_data,params]=loadRawDataBlock("./radarData.blob","radarParameters.json");
+raw_data=raw_data';
+[sweeps,azimuth_samples]=size(raw_data);
+
+%% Params
+
+c=3e8;
+fc=params.carrierFreq; % carrier
+B=params.bandwidth; % Bandwidth
+T=0.5e-3; % Chirp time
+Alfa=B/T; % slope
+ant_angle=5; %antenna aperture angle (default to 20)
+v=params.averageVelocity; % platform's velocity
+PRI=T; % Pulse repetition interval, assume one pulse
+PRF=1/PRI;
+max_range=1500;% max range of radar, used to calculate antenna max width and
+% azimuth reference functions
+fb_max=max_range*2*Alfa/c;
+
+
+Bd=2*v/c*fc;
+
+
+fs=params.samplingFreq;
+t=0:1/fs:T-1/fs;
+samples=length(t);
+
+% azimuth_samples=p;
+azimuth_distance=azimuth_samples*PRI*v;
+azimuth_step=azimuth_distance/azimuth_samples;
+
+radar=radar_object(B,T,fc,v,PRI,ant_angle,fb_max);
+%%Bd=2*v/radar.lambda*deg2rad(ant_angle);
+
+
+radar.SAR_raw_data=raw_data;
 
 %% Range compression
 radar.SAR_range_compressed=range_compression(radar.SAR_raw_data);
+display_range_compressed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 %% RCMC
 radar.SAR_range_doppler=range_doppler_transform(radar.SAR_range_compressed);
